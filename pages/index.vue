fae8d52e16dc72242138a46a45a2f60ddf56e89d
CezzMartins <cezzmartins@gmail.com>
2021-10-18 20:22:53 -0300

    feat: auto import components nad loop

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,20 @@
 <template>
     <div>
-        <h1>Hello World</h1>
+        <div v-for="home in homes" :key="home.objectID" style="float: left; margin: 10px;">
+            <HomeCard :home="home"/>
+        </div>
     </div>
 </template>
 
 <script>
+import homes from '@/data/homes.json';
     export default {
         
+        data(){
+            return{
+                homes: homes.slice(0,4)
+            }
+        }
     }
 </script>
 
