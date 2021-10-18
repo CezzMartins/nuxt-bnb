6299bd52463fb37013b254710e2cd3e78d7178d5
CezzMartins <cezzmartins@gmail.com>
2021-10-18 20:55:21 -0300

    feat: add images and icons

--- pages/home/_id.vue
+++ pages/home/_id.vue
@@ -1,12 +1,24 @@
 <template>
     <div>
-        <h1>{{ home.title }}</h1>
+        <div style="display: flex">
+            <img v-for="image in home.images" :key="image" alt="homes" :src="image" width="200px" height="150px"/>        
+        </div>
+    {{ home.title }}
+    ${{ home.pricePerNight }} / night <br/>
+    <img src="/images/marker.svg" alt="" width="20" height="20">{{ home.location.address }} - {{ home.location.city }} {{ home.location.state }} {{ home.location.country }}<br/>
+    <img src="/images/star.svg" alt="" width="20" height="20">{{ home.reviewValue }} <br/>
+    {{ home.guests }} guests, {{ home.badrooms }}, {{ home.beds }} beds, {{ home.bathrooms }} bath<br/>
     </div>
 </template>
 
 <script>
 import homes from '@/data/homes.json';
     export default {
+        head(){
+            return{
+                title: this.home.title
+            }
+        },
         data(){
             return{
                 home: {}
