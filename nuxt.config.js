049225d04d61a3de5ad3e5b055c379a89327ad57
CezzMartins <cezzmartins@gmail.com>
2021-10-18 20:40:17 -0300

    feat: get data from json

---
+++ nuxt.config.js
@@ -0,0 +1,17 @@
+export default{
+    // Seo Meta tags
+    head: {
+        titleTemplate: "Nuxt BnB",
+        htmlAttrs: {
+            lang: "en"
+        },
+        bodyAttrs:{
+            class:["my-style"]
+        },
+        meta: [{
+            charset: "utf-8",
+        }]
+    },
+    //auto import components
+    components: true,
+}
---
+++ pages/home/_id.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <h1>{{ home.title }}</h1>
+    </div>
+</template>
+
+<script>
+import homes from '@/data/homes.json';
+    export default {
+        data(){
+            return{
+                home: {}
+            }
+        },
+        created(){
+            const home = homes.find((home) => home.objectID == this.$route.params.id)
+            console.log(this.$route)
+            this.home = home
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
