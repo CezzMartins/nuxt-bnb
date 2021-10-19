a2e0177241dc796b0c35c1385ec9680a018fd470
CezzMartins <cezzmartins@gmail.com>
2021-10-18 23:38:24 -0300

    feat: add Google Maps

--- pages/home/_id.vue
+++ pages/home/_id.vue
@@ -9,6 +9,9 @@
     <img src="/images/marker.svg" alt="" width="20" height="20">{{ home.location.address }} - {{ home.location.city }} {{ home.location.state }} {{ home.location.country }}<br/>
     <img src="/images/star.svg" alt="" width="20" height="20">{{ home.reviewValue }} <br/>
     {{ home.guests }} guests, {{ home.badrooms }}, {{ home.beds }} beds, {{ home.bathrooms }} bath<br/>
+    <div style="height: 800px; width: 800px" ref="map">
+
+    </div>
     </div>
 </template>
 
@@ -17,18 +20,38 @@ import homes from '@/data/homes.json';
     export default {
         head(){
             return{
-                title: this.home.title
+                title: this.home.title,
+                script: [{
+                    src: "https://maps.googleapis.com/maps/api/js?key=AIzaSyANnbr08pT--z5OBnhiZBwxShBY3_oOjQM&libraries=places",
+                    hid: "map",
+                    defer: true
+                }]
             }
         },
         data(){
             return{
-                home: {}
+                home: {},
+                api: process.env.API_KEY
             }
         },
         created(){
             const home = homes.find((home) => home.objectID == this.$route.params.id)
-            console.log(this.$route)
             this.home = home
+        },
+        mounted(){
+            const mapOptions = {
+                zoom:18,
+                center: new window.google.maps.LatLng(this.home._geoloc.lat, this.home._geoloc.lng),
+                disableDefaultUI: true,
+                zoomControl: true,
+                
+            }
+            const map = new window.google.maps.Map(this.$refs.map, mapOptions)
+            const position = new window.google.maps.LatLng(this.home._geoloc.lat, this.home._geoloc.lng)
+            const marker = new window.google.maps.Marker({
+                position: position,
+            })
+            marker.setMap(map)
         }
     }
 </script>
